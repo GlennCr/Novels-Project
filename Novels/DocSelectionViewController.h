7a3f18e6fe3ec59d7ce1736246b30a890429908e
Glenn <gtcaccs@gmail.com>
2012-01-26 13:55:05 -0700

    Standard first commit

---
+++ Novels/DocSelectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  DocSelectionViewController.h
+//  Novels
+//
+//  Created by asuuser on 1/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DocSelectionViewController : UIViewController
+
+@end
---
+++ Novels/TextViewController.h
@@ -0,0 +1,13 @@
+//
+//  TextViewController.h
+//  Novels
+//
+//  Created by asuuser on 1/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextViewController : UIViewController
+
+@end
